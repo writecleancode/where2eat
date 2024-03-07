8e6d740391bb214b737febb3da94afae2e44e904
Rafal <rafek131@wp.pl>
2024-03-07 18:53:30 +0100

    styles fix: 'NavLinksFilter' component is now displayed correctly on devices with screen with >1020px & <1400px

--- src/components/atoms/NavButtonsWrapper/NavButtonsWrapper.ts
+++ src/components/atoms/NavButtonsWrapper/NavButtonsWrapper.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 export const NavButtonsWrapper = styled.div<{ $isDesktop?: boolean }>`
-	display: ${({ $isDesktop }) => ($isDesktop ? 'none' : 'flex')};
+	display: flex;
 	flex-direction: column;
 	gap: 0.8rem;
 
 	@media (min-width: 600px) {
 		padding: 1.6rem 0;
+	}
+
+	@media (min-width: 1400px) {
+		${({ $isDesktop }) => ($isDesktop ? 'display: none;' : '')};
 	}
 `;
